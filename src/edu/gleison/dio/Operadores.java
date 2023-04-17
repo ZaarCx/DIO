36786e57cd928391f49ceb7c6afb6b96cd55d900
ZaarCx <111471135+ZaarCx@users.noreply.github.com>
2023-04-17 19:44:07 -0300

    Atualização

--- src/edu/gleison/dio/Operadores.java
+++ src/edu/gleison/dio/Operadores.java
@@ -4,9 +4,12 @@ public class Operadores {
     public static void main(String[] args) {
 
         int numero = 5;
+        boolean variavel = false;
+        numero++;
+        System.out.println(- numero);
         numero++;
         System.out.println(numero);
-        numero++;
-        System.out.println(numero);
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
