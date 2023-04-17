f800ca9820ca7bb23e694eb7d61b7718e981bf14
ZaarCx <111471135+ZaarCx@users.noreply.github.com>
2023-04-17 15:42:00 -0300

    Atualização

--- src/edu/gleison/dio/TiposVariaveis.java
+++ src/edu/gleison/dio/TiposVariaveis.java
@@ -1,4 +1,13 @@
 package edu.gleison.dio;
 
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        // tipos primitivos
+        short var1 = 10;
+
+        final double VAR = var1;
+
+
+        System.out.println(VAR);
+    }
 }
