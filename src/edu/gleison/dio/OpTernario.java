ec8eb43f93106e26da294441fba78fd1548489f0
ZaarCx <111471135+ZaarCx@users.noreply.github.com>
2023-04-18 13:57:15 -0300

    Atualização

--- src/edu/gleison/dio/OpTernario.java
+++ src/edu/gleison/dio/OpTernario.java
@@ -8,9 +8,21 @@ public class OpTernario {
         int a,b;
         a = 5;
         b = 5;
+
         String resultado = a == b ? "verdadeiro" : "falso";
 
         System.out.println(resultado);
+
+       if (resultado == "verdadeiro"){
+           System.out.println("você passou!");
+       } else {
+           System.out.println("você reprovou!");
+       }
+       if (a > b){
+           System.out.println("Aprovado");
+       } else {
+           System.out.println("mas foi reprovado!");
+       }
 
     }
 }
