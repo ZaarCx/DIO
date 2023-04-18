f9aa5bd157394e662881bf8a752a5ad1572b6a39
ZaarCx <111471135+ZaarCx@users.noreply.github.com>
2023-04-18 14:26:33 -0300

    Atualização

--- src/edu/gleison/dio/OpTernario.java
+++ src/edu/gleison/dio/OpTernario.java
@@ -5,8 +5,9 @@ import java.beans.PropertyEditorSupport;
 public class OpTernario {
     public static void main(String[] args) {
 
-        int a,b;
-        a = 5;
+
+        final int a = 7;
+        int b;
         b = 5;
 
         String resultado = a == b ? "verdadeiro" : "falso";
@@ -16,13 +17,27 @@ public class OpTernario {
        if (resultado == "verdadeiro"){
            System.out.println("você passou!");
        } else {
-           System.out.println("você reprovou!");
+           System.out.println("você ficou abaixo da média!");
        }
        if (a > b){
-           System.out.println("Aprovado");
+           System.out.println("mas foi aprovado");
        } else {
            System.out.println("mas foi reprovado!");
        }
+
+       int numero = 1;
+       int numero1 = 2;
+
+       boolean simNao = numero == numero1;
+
+        System.out.println("o numero é diferente do numero1? \n" + simNao);
+
+        if (simNao == false){
+            System.out.println("sim, é diferente!");
+        } else {
+            System.out.println("é igual");
+        }
+
 
     }
 }
